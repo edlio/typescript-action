6bf4dfff0caad818e46e96466f9d300006172cae
Oguz Bilgic <fisyonet@gmail.com>
2019-10-13 01:33:28 -0700

    Add annotation functionality

--- compile.js
+++ compile.js
@@ -1,9 +1,41 @@
 const ts = require('typescript')
 const fs = require('fs')
+const request = require('./request')
 
-const { GITHUB_WORKSPACE } = process.env
+const { GITHUB_WORKFLOW, GITHUB_SHA, GITHUB_EVENT_PATH, GITHUB_TOKEN, GITHUB_WORKSPACE } = process.env
+const event = require(GITHUB_EVENT_PATH)
+
+const { repository } = event
+const {
+  owner: { login: owner }
+} = repository
+const { name: repo } = repository
 
 const jsconfig = require(`${GITHUB_WORKSPACE}/jsconfig.json`)
+
+const headers = {
+  'Content-Type': 'application/json',
+  Accept: 'application/vnd.github.antiope-preview+json',
+  Authorization: `Bearer ${GITHUB_TOKEN}`,
+  'User-Agent': 'eslint-action'
+}
+
+async function createCheck() {
+  const body = {
+    name: GITHUB_WORKFLOW,
+    head_sha: GITHUB_SHA,
+    status: 'in_progress',
+    started_at: new Date()
+  }
+
+  const { data } = await request(`https://api.github.com/repos/${owner}/${repo}/check-runs`, {
+    method: 'POST',
+    headers,
+    body
+  })
+
+  return data.id
+}
 
 /**
  * Returns all files within the folder excep . folders and node_modules
@@ -20,7 +52,7 @@ function getFiles(dir, files_ = []) {
     // Don't go into hidden folders such as .git
     if (fs.statSync(name).isDirectory() && !name.includes('/.')){
       getFiles(name, files_);
-    // don't include non-js files and skip node_modules
+      // don't include non-js files and skip node_modules
     } else if (name.endsWith('.js') && !name.includes('node_modules') ) {
       files_.push(name);
     }
@@ -41,25 +73,91 @@ function compile(fileNames, options) {
     .getPreEmitDiagnostics(program)
     .concat(emitResult.diagnostics);
 
+  const annotations = []
   allDiagnostics.forEach(diagnostic => {
     if (diagnostic.file) {
       const { line, character } = diagnostic.file.getLineAndCharacterOfPosition(
         diagnostic.start
       );
+
       const message = ts.flattenDiagnosticMessageText(
         diagnostic.messageText,
         "\n"
       );
+
       console.log(
         `${diagnostic.file.fileName} (${line + 1},${character + 1}): ${message}`
       );
+
+      const path = diagnostic.file.fileName.substring(GITHUB_WORKSPACE.length + 1)
+
+      annotations.push({
+        path,
+        start_line: line,
+        end_line: line,
+        annotation_level: 'failure',
+        message: `${message}`
+      })
     } else {
       // console.log( `${ts.flattenDiagnosticMessageText(diagnostic.messageText, "\n")}`);
     }
   });
 
-  const exitCode = emitResult.emitSkipped ? 1 : 0;
-  process.exit(exitCode);
+  return {
+    conclusion: emitResult.emitSkipped > 0 ? 'failure' : 'success',
+    output: {
+      title: GITHUB_WORKFLOW,
+      summary: `${annotations.length} error(s) found`,
+      annotations
+    }
+  }
 }
 
-compile(getFiles('.'), jsconfig.compilerOptions);
+async function updateCheck(id, conclusion, output) {
+  if (output) {
+    output.annotations = output.annotations.slice(0, 50)
+  }
+
+  const body = {
+    name: GITHUB_WORKFLOW,
+    head_sha: GITHUB_SHA,
+    status: 'completed',
+    completed_at: new Date(),
+    conclusion,
+    output
+  }
+
+  await request(`https://api.github.com/repos/${owner}/${repo}/check-runs/${id}`, {
+    method: 'PATCH',
+    headers,
+    body
+  })
+}
+
+function exitWithError(err) {
+  console.error('Error', err.stack)
+  if (err.data) {
+    console.error(err.data)
+  }
+  process.exit(1)
+}
+
+async function run() {
+  const id = await createCheck()
+  try {
+    const files = getFiles('.')
+    const { conclusion, output } = compile(files, jsconfig.compilerOptions);
+
+    console.log(output.summary)
+
+    await updateCheck(id, conclusion, output)
+    if (conclusion === 'failure') {
+      process.exit(78)
+    }
+  } catch (err) {
+    await updateCheck(id, 'failure')
+    exitWithError(err)
+  }
+}
+
+run().catch(exitWithError)
