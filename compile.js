f15c43df295cf307295887bcd027ae4d3d450585
Oguz Bilgic <fisyonet@gmail.com>
2019-10-13 01:08:17 -0700

    Add compile.js

---
+++ compile.js
@@ -0,0 +1,63 @@
+const ts = require('typescript')
+const fs = require('fs')
+
+const jsconfig = require('./jsconfig.json')
+
+/**
+ * Returns all files within the folder excep . folders and node_modules
+ *
+ * @param {string} dir
+ * @param {string[]=} files_
+ * @returns {string[]}
+ */
+function getFiles(dir, files_ = []) {
+  const files = fs.readdirSync(dir);
+
+  for (var i in files){
+    const name = dir + '/' + files[i];
+    // Don't go into hidden folders such as .git
+    if (fs.statSync(name).isDirectory() && !name.includes('/.')){
+      getFiles(name, files_);
+    // don't include non-js files and skip node_modules
+    } else if (name.endsWith('.js') && !name.includes('node_modules') ) {
+      files_.push(name);
+    }
+  }
+
+  return files_;
+}
+
+/**
+ * @param {string[]} fileNames
+ * @param {Object} options
+ */
+function compile(fileNames, options) {
+  const program = ts.createProgram(fileNames, options);
+  const emitResult = program.emit();
+
+  const allDiagnostics = ts
+    .getPreEmitDiagnostics(program)
+    .concat(emitResult.diagnostics);
+
+  allDiagnostics.forEach(diagnostic => {
+    if (diagnostic.file) {
+      const { line, character } = diagnostic.file.getLineAndCharacterOfPosition(
+        diagnostic.start
+      );
+      const message = ts.flattenDiagnosticMessageText(
+        diagnostic.messageText,
+        "\n"
+      );
+      console.log(
+        `${diagnostic.file.fileName} (${line + 1},${character + 1}): ${message}`
+      );
+    } else {
+      // console.log( `${ts.flattenDiagnosticMessageText(diagnostic.messageText, "\n")}`);
+    }
+  });
+
+  const exitCode = emitResult.emitSkipped ? 1 : 0;
+  process.exit(exitCode);
+}
+
+compile(getFiles('.'), jsconfig.compilerOptions);
--- entrypoint.sh
+++ entrypoint.sh
@@ -6,4 +6,7 @@ npm install --no-package-lock
 
 npm install -g typescript
 
-tsc --project jsconfig.json
+# tsc --project jsconfig.json
+
+pwd
+node /action/compile.js
